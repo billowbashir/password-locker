999e385eca8b14ee0eb25d4134d1be14245c43a8
billowbashir <billowbashir@gmail.com>
2018-08-27 15:05:38 +0300

    update run.py to prompt user to create account

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3.6
 import random
 from credentials import Credentials
-def create_account(name,uname,password):
+def create_account(name,username,passcode):
     new_user=User (name,uname,password)
     return new_user
+def sav_user(user):
+    user.save_user()
 def create_credential(pname,uname,password):
     new_credential=Credentials(pname,uname,password)
     return new_credential # create new credential and return it
@@ -18,11 +20,19 @@ def check_existing_credentials(platform_name):
 def show_credentials():
     return Credentials.display_all_credentials()
 def main():
-    print("Hello Welcome to password locker. What is your name?")
-    user_name = input()
+    print("Hello Welcome to password locker. fill in the details below to create an account")
+    print("full name....")
+    name = input()
+    print("username ....)
+    username = input()
+    print("password....")
+    passcode=input()
+    print("confirm password ....")
+    confirmpassword = input()
+    if confirmpassword==passcode:
 
-    print(f"Hello {user_name}. what would you like to do?")
-    print('\n')
+        print(f" account succesfully created welcome {username}. what would you like to do?")
+        print('\n')
 
     while True:
         print("Use these short codes : gp - generate a new password, dp - display saved passwords, fp -find a password, ex -exit password locker ")
