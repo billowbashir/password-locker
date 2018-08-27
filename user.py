e93d3a29359d4d94cfa2ba478ed340cb7e95e41b
billowbashir <billowbashir@gmail.com>
2018-08-27 14:39:18 +0300

    add an empty array of user_list for the user info

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 import unittest
 class User:
-        def __init__(self,name,username,password):
-            self.name=name
-            self.username=username
-            self.password=password
+    user_list=[]
+    def __init__(self,name,username,password):
+        self.name=name
+        self.username=username
+        self.password=password
